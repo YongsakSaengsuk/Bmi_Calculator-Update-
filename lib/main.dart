9053bd1eb4616ca2da00110fdd56444a39692177
YongsakSaengsuk <Yongsaksaengsuk@gmail.com>
2025-01-20 01:23:30 +0700

    mobile_web_show??

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:bmi_calculator/bmi_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_frame/flutter_web_frame.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: BmiPage(),
-    debugShowCheckedModeBanner: false,
-  ));
+  runApp(FlutterWebFrame(
+      builder: (context) {
+        return MaterialApp(
+          home: BmiPage(),
+          debugShowCheckedModeBanner: false,
+        );
+      },
+      maximumSize: Size(475.0, 812.0)));
 }
